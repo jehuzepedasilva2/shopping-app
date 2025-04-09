76e5534ac5cb8704fcdf2797ba7836844ba28540
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-04-09 01:08:31 -0700

    cart page display items in cart;

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,17 @@
 import '../styles/cart-page.css';
 import '../styles/common.css';
+import { CartContext } from '../layouts/MainLayout.jsx';
+import { useContext } from 'react';
 
 const CartPage = () => {
+
+  const { cartDetails, _ } = useContext(CartContext);
+
   return (
     <div>
       <h1 className='main-headings'>Cart</h1>
+      <h2>Number of Items: {cartDetails.numItems}</h2>
+      <h2>Total: {cartDetails.total}</h2>
     </div>
   )
 }
