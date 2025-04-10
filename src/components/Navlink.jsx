9f05738fc178ecda928126e5780b47fb62c9dbd8
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-04-09 17:26:50 -0700

    added icon to top of cart nav link to notify user how many items are in the cart

--- src/components/Navlink.jsx
+++ src/components/Navlink.jsx
@@ -1,6 +1,11 @@
 import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../layouts/MainLayout.jsx';
 
 export default function Navlink({ title, to, classFunction }) {
+
+  const { cartDetails, _ } = useContext(CartContext);
+
   return (
    <>
     <NavLink 
@@ -8,6 +13,9 @@ export default function Navlink({ title, to, classFunction }) {
       className={({ isActive }) => classFunction(isActive)}
     >
       {title}
+      {title === 'CART' && 
+        <div className='item-count'>{cartDetails.items.length}</div>
+      }
     </NavLink>
    </>
   )
